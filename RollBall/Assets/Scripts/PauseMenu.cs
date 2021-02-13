ad28b906cd943300c528892e0dafc6ea9a97000d
Kyle Lemons <kyllemonhead@aol.com>
2021-02-12 21:10:52 -0600

    Upload of Roll A Ball

---
+++ RollBall/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public GameObject SettingsMenuUI;
+
+    public Slider speedslider;
+
+    public GameObject Player;
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Keyboard kb = InputSystem.GetDevice<Keyboard>();
+
+        if (kb.escapeKey.wasPressedThisFrame)
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadSettings()
+    {
+        pauseMenuUI.SetActive(false);
+        Debug.Log("Loading settings");
+        SettingsMenuUI.SetActive(true);
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading menu");
+        //SceneManager.LoadScene("");
+    }
+
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+
+    }
+
+    public void Back()
+    {
+        pauseMenuUI.SetActive(true);
+        SettingsMenuUI.SetActive(false);
+    }
+
+    public void ToggleChanged(bool isToggled)
+    {
+        GameObject sphere = GameObject.FindWithTag("Player");
+        if (isToggled)
+        {
+            sphere.transform.localScale = new Vector3(1, 1, 1);
+
+        }
+        else
+        {
+
+            sphere.transform.localScale = new Vector3(2, 2, 2);
+        }
+    }
+    
+    
+
+}
---
+++ RollBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20;
+    public TextMeshProUGUI countText;
+
+    
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    public Slider speedslider;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+
+
+
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            SceneManager.LoadScene("End");
+        }
+    }
+    void Update()
+    {
+        Keyboard kb = InputSystem.GetDevice<Keyboard>();
+
+        if (kb.escapeKey.wasPressedThisFrame)
+        {
+            //Time.timeScale = 0;
+            //Time.timeScale = 1
+            Debug.Log("Hello");
+        }
+
+    }
+    void FixedUpdate()
+    {
+        speed = speedslider.value;
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+        
+    }
+
+    public void DropDownInput(int val)
+    {
+        GameObject myplayer = GameObject.FindGameObjectWithTag("Player");
+
+
+        if (val == 0)
+        {
+            myplayer.GetComponent<Renderer>().material.color = Color.blue;
+
+        }
+        if (val == 1)
+        {
+            myplayer.GetComponent<Renderer>().material.color = Color.red;
+
+        }
+        if (val == 2)
+        {
+            myplayer.GetComponent<Renderer>().material.color = Color.yellow;
+
+        }
+    }
+    
+}
